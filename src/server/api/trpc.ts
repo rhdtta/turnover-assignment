fa3bc238a57044a8f3f74e650b3d6f649bafc6df
Rohit Dutta <rohitdutta@Rohits-MacBook-Air.local>
2024-03-30 17:22:34 +0530

    re - some changes regarding cookie extracting logic

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -36,7 +36,7 @@ const isAuthed = t.middleware(async ({ctx, next}) =>{
   const cookies = ctx.req.headers.get('cookie')?.split(";");
 
   const ourCookie = cookies?.filter((cookie) => cookie.split("=")[0] === 'token');
-  let token = ourCookie? ourCookie[0]?.split("=")[1] : '';
+  const token = ourCookie? ourCookie[0]?.split("=")[1] : '';
 
   if(token?.length) {
     const payload = await decrypt(token);
